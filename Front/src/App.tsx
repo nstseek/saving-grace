c57f47e4c20cc4baaf616f9051d4fbe76496883b
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2020-10-03 19:52:43 -0300

    Adiciona exemplo de utilização do Material UI

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,10 +1,13 @@
+import { Card, CardContent } from '@material-ui/core';
 import React from 'react';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Saving Grace app working!
+    <div className='App'>
+      <Card>
+        <CardContent>Saving Grace app working!</CardContent>
+      </Card>
     </div>
   );
 }
